949bcc805e4652d28b9f3d75deea861a5c2a2b5c
LoganZ2 <loganzhuang123@gmail.com>
2024-04-06 00:38:49 -0400

    Made Plane subclass of Body

---
+++ src/Plane.java
@@ -0,0 +1,9 @@
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.geometry.Vector2;
+
+public class Plane extends Body {
+    public void move(double x, double y) {
+        this.applyForce(new Vector2(x*1000, y*1000));
+        this.getTransform().setRotation(0);
+    }
+}
